319cf7429ac2996265d848315464ea1c4d0eb237
dbelford <dbelford@gmail.com>
2015-08-17 22:25:47 -0700

    Delete cruft
    Delete cruft and comments from settings controller.

--- NumpadRemap/Classes/NRNumpadSettingsController.m
+++ NumpadRemap/Classes/NRNumpadSettingsController.m
@@ -25,6 +25,8 @@
 
 @implementation NRNumpadSettingsController
 
+#pragma mark - Init Methods
+
 - (id)init {
     
     self = [self initWithNibName:nil bundle:nil];
@@ -36,7 +38,7 @@
     self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
     if (self) {
         // Initialization code here.
-        [self setupObject];
+
     }
     return self;
 }
@@ -44,11 +46,13 @@
 - (id)initWithCoder:(NSCoder *)aDecoder {
     self = [super initWithCoder:aDecoder];
     if (self) {
-        [self setupObject];
+        
     }
 
     return self;
 }
+
+#pragma mark - Responding to Key Presses
 
 - (BOOL)acceptsFirstResponder {
     return YES;
@@ -78,40 +82,12 @@
     
 }
 
-- (void)setupObject {
-//    self.numpadModel = [[NRNumpadModel alloc] init];
-//    NRNumpadViewModel *vm = [[NRNumpadViewModel alloc] init];
-//    vm.model = self.numpadModel;
-//    self.numpadView.viewModel = vm;
-    
-//    self.monitor = [[DABActiveApplicationsMonitor alloc] init];
-//    [self updateRunningApps];
-}
-
-- (void)updateRunningApps {
-    
-    [self.monitor updateApplicationData];
-    
-//    [self.monitor orderedApps];
-    
-//    NSArray *runningApps = [[NSWorkspace sharedWorkspace] runningApplications];
-    NSArray *runningApps = self.monitor.orderedRunningApplications;
-    
-    runningApps = [runningApps reject:^ BOOL (NSRunningApplication *app){
-        
-        return app.activationPolicy != NSApplicationActivationPolicyRegular;
-        
-    }];
-    NSLog(@"The running apps: %@", runningApps);
-    
-}
+#pragma mark - Configure Controller
 
 - (void)awakeFromNib {
     [super awakeFromNib];
     
-//    __block int i = 0;
     [self configurePreferencePane];
-//    [self configureKeyViews];
     [self configureConstraints];
     
     self.numpadModel = [[NRNumpadModel alloc] init];
@@ -124,6 +100,14 @@
     }];
 }
 
+- (void)loadView {
+    [super loadView];
+    
+    self.nextResponder = self.numpadView.nextResponder;
+    self.numpadView.nextResponder = self;
+    
+}
+
 - (void)configurePreferencePane {
     NSArray *topLevel;
     [[NSBundle mainBundle] loadNibNamed:@"NRNumpadPreferencesView" owner:nil topLevelObjects:&topLevel];
@@ -133,39 +117,6 @@
     
     [self.view addSubview:self.prefView];
 
-}
-
-- (void)configureKeyViews {
-//    [self.numpadModel update];
-//    NSArray *keyOrder = @[@(kVK_ANSI_Keypad7),
-//                          @(kVK_ANSI_Keypad8),
-//                          @(kVK_ANSI_Keypad9),
-//                          @(kVK_ANSI_Keypad4),
-//                          @(kVK_ANSI_Keypad5),
-//                          @(kVK_ANSI_Keypad6),
-//                          @(kVK_ANSI_Keypad1),
-//                          @(kVK_ANSI_Keypad2),
-//                          @(kVK_ANSI_Keypad3),
-//                          @(kVK_ANSI_Keypad0)
-//                          ];
-//    self.numpadView.keyViews = [self.numpadModel.numpadKeys map:^ NSView* (NSNumber *key, NRNumpadShortcutModel *shortcut){
-//        NRNumpadKeyView *view = [[NRNumpadKeyView alloc] initWithFrame:NSMakeRect(0, 0, 400, 400)];
-//        view.iconImageView.image = shortcut.image;
-//        view.keyLabel.stringValue = shortcut.keyCodeString;
-//        view.identifier = key.stringValue;
-//        
-//        return view;
-//    }];
-//    
-//    self.numpadView.keyViews = [self.numpadView.keyViews sortedArrayWithOptions:0 usingComparator:^NSComparisonResult (NRNumpadKeyView *view1, NRNumpadKeyView *view2) {
-//        NSInteger v1 = view1.identifier.integerValue;
-//        NSInteger v2 = view2.identifier.integerValue;
-//        
-//        NSUInteger v1Ordered = [keyOrder indexOfObject:@(v1)];
-//        NSUInteger v2Ordered = [keyOrder indexOfObject:@(v2)];
-//        
-//        return v1Ordered < v2Ordered ? NSOrderedAscending : NSOrderedDescending;
-//    }];
 }
 
 - (void)configureConstraints {
@@ -191,14 +142,5 @@
     }];
 }
 
-- (void)loadView {
-    [super loadView];
-    
-    self.nextResponder = self.numpadView.nextResponder;
-    self.numpadView.nextResponder = self;
-    
-    [self updateRunningApps];
-    
-}
 
 @end
