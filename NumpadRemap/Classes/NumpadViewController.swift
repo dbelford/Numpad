bd54f22215d7f2e4ec804376e4525b47bc0c4450
dbelford <dbelford@gmail.com>
2018-08-17 16:07:48 -0700

    Intermediate edits..

---
+++ NumpadRemap/Classes/NumpadViewController.swift
@@ -0,0 +1,9 @@
+//
+//  NumpadViewController.swift
+//  Numpad
+//
+//  Created by David Belford on 8/14/18.
+//  Copyright © 2018 David Belford. All rights reserved.
+//
+
+import Foundation
---
+++ NumpadRemap/WindowController.swift
@@ -0,0 +1,9 @@
+//
+//  WindowController.swift
+//  Numpad
+//
+//  Created by David Belford on 8/15/18.
+//  Copyright © 2018 David Belford. All rights reserved.
+//
+
+import Foundation
